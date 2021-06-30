e33e5eaf77d9c430dfd130796d4064b6cee399bf
Emil Ståhl <emil.stahl@icloud.com>
2021-06-30 14:42:08 +0200

    Create parse_vtysh_data_installed_prefixes.py

---
+++ Python scripts/parse_vtysh_data_installed_prefixes.py
@@ -0,0 +1,59 @@
+import json
+import sys
+
+def parse():
+
+    with open('capture.json') as json_file:
+        data = json.load(json_file)
+
+        pfxRcd = 0 
+        pfxSnt = 0
+        msgSent = 0
+        inQ = 0
+        outQ = 0
+        peersEstablished = 0
+        receivedPrefixes = []
+        for value in data['ipv4Unicast']['peers'].values():
+            pfxRcd += int(value["pfxRcd"])
+            msgSent += int(value["msgSent"])
+            inQ += int(value["inq"])
+            outQ += int(value["outq"])
+            if (value["state"] == "Established"):
+                pfxSnt = int(value["pfxSnt"])
+                receivedPrefixes.append(int(value["pfxRcd"]))
+                peersEstablished += 1
+                
+
+    receivedPrefixes = sorted(receivedPrefixes)
+    res = ((receivedPrefixes[-1] - receivedPrefixes[0])/receivedPrefixes[0])*100
+    print(f"Results for {sys.argv[1]} peers and round {sys.argv[3]}:\n")
+    print("Number of peers established: " + str(peersEstablished))
+    print("Prefixes in InQ: " + str(inQ))
+    print("Prefixes in OutQ: " + str(outQ))
+    print("Total message sent: " + str(msgSent))
+    print("Total prefixes received: " + str(pfxRcd))
+    print("Total prefixes sent: " + str(pfxSnt))
+    print("Difference between received and sent prefixes: " + str(int(pfxRcd-pfxSnt)))
+    print("Difference between most and least received prefixes: " + str(res) + "%")
+
+    filename = f"vtyshresults-{sys.argv[2]}peers-{sys.argv[1]}.txt"
+    filename2 = f"vtysh-pfxsnt-{sys.argv[2]}peers-{sys.argv[1]}.txt"
+    results = open(filename, "a")
+    pfxSntResults = open(filename2, "a")
+
+    results.write(f"Results for {sys.argv[2]} peers and round {sys.argv[3]}:\n")
+    results.write("Number of peers established: " + str(peersEstablished) + "\n")
+    results.write("Prefixes in InQ: " + str(inQ) + "\n")
+    results.write("Prefixes in OutQ: " + str(outQ) + "\n")
+    results.write("Total message sent: " + str(msgSent) + "\n")
+    results.write("Total prefixes received: " + str(pfxRcd) + "\n")
+    results.write("Total prefixes sent: " + str(pfxSnt) + "\n")
+    results.write("Difference between received and sent prefixes: " + str(int(pfxRcd-pfxSnt)) + "\n")
+    results.write("Delta received prefixes: " + str(res) + "%\n")
+    results.write("\n")
+
+    pfxSntResults.write(str(pfxSnt) + "\n")
+
+
+if __name__ == "__main__":
+    parse()
